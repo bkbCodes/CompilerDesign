bf3e8e19365c1c7f70e029db7b3e36a3816acf35
DaivikCodes <Daivik.Gupta.DG@gmail.com>
2022-08-27 23:23:13 +0530

    Grouping common production and creating new production to reduce non-deterministic nature

--- NDG_to_DG/conversion.py
+++ NDG_to_DG/conversion.py
@@ -1,25 +1,72 @@
 # Non deterministic grammar to deterministic grammar converions
 
 # Global variables:
-productions = {}    # store produtions rules {symbols: rules} eg. A->a/b is stored as {"A":["a","b"]}
-symbols = []        # variable ie non terminal symbols
-terminals = []      # terminals
+filename = "NDG_to_DG/Grammar"    # file to be read
+file = open(filename)   # open the specified file and read the Grammar
+productions = {}        # store produtions rules {symbols: rules} eg. A->a/b is stored as {"A":["a","b"]}
+symbols = []            # variable ie non terminal symbols
+terminals = []          # terminals
+startSymbol = ""
 
-print("Type 'epsilon' for epsilon production")
-print("Enter Non-Deterministic Grammar: ")
-line = input()
 
 # getting Production rules
-while ":wq" not in line:
+for line in file:
     production = line.split("->")
-    print(production)
     current_symbol = production[0]
-    rules = production[1].split("/")
-    symbols.append(symbols)
+
+    # setting the first Symbol as startSymbol
+    if startSymbol == "":
+        startSymbol = current_symbol
+
+    rules = production[1].split("\n")[0].split('/')
+    symbols.append(current_symbol)
     productions[current_symbol] = rules
-    line = input()
+
+    # print the production and its length
+    print(productions[current_symbol])
+    print(len(productions[current_symbol]))
+
 
 # getting Start Symbol of the grammar
-startSymbol = input("Start symbol: ")
+startSymbol = input("Start symbol: ") if "y" in input("Custom Start Symbol (y/n): ").lower() else startSymbol
+if startSymbol not in symbols:
+    raise Exception(ValueError("Invalid Start Symbol"))
 
-print(productions, startSymbol)
+
+print(productions, startSymbol, symbols)
+
+
+for s in symbols:
+    # if symbol 's' has only 1 rule the skip to next symbol
+    if len(productions[s]) < 2:
+        continue
+
+    j=1
+    # get the prodution that is common in rules
+    common = productions[s][0][:j]
+
+    # create new symbol s' for new productions
+    newSymbol = s+"'"
+    symbols.append(newSymbol)
+    productions[newSymbol] = []
+    popInds = []
+
+    for prodNo in range(len(productions[s])):
+        prod = productions[s][prodNo]
+        if prod == None:
+            continue
+        if common == prod[:j]:
+            if len(prod) <2:
+                productions[newSymbol].append(None)
+            else:
+                productions[newSymbol].append(prod[j:])
+            popInds.append(prodNo)
+    
+    productions[s].append(common+newSymbol)
+
+    for i in range(len(popInds)):
+        productions[s].pop(popInds[i] - i)
+
+
+for i in productions:
+    print(i +"\t", productions[i])
